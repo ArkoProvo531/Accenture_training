dc317ca42dc1eed12589b4cc46cba6d168b8e69f
Arko Provo Roy <61874382+ArkoProvo531@users.noreply.github.com>
2023-04-26 10:26:36 +0530

    Update storage-account.tf

--- storage-account.tf
+++ storage-account.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "example" {
-  name                     = "arkoroy123"
+  name                     = "arkoroy1234"
   resource_group_name      = "github"
   location                 = "West Europe"
   account_tier             = "Standard"
